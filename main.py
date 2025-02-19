09b2b9dc370c80fd4dd3e038511282c928862f38
Ilse547 <88056558+Ilse547@users.noreply.github.com>
2025-02-19 15:02:10 +0100

    add py

---
+++ main.py
@@ -0,0 +1,3 @@
+
+	ioid init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	ptivoid init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
